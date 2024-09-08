49e9b630cd44716b84304aca1177f9b7cbafdddd
Gabriel Mariano <gabriel.mariano1497@gmail.com>
2024-09-08 15:11:53 -0300

    Add files via upload

---
+++ dados.js
@@ -0,0 +1,18 @@
+let dados = [
+    {
+        titulo: "Renda Fixa",
+        descricao: "renda fixa é uma modalidade de investimento em que a rentabilidade é pré-determinada no momento da aplicação. Ou seja, o investidor sabe exatamente quanto irá ganhar ao final do prazo, desde que mantenha o investimento até o vencimento. Essa característica a torna uma opção mais conservadora para quem busca segurança e previsibilidade nos seus investimentos. Na renda fixa, o investidor, na prática, está emprestando dinheiro para o emissor do título (que pode ser o governo, uma empresa ou um banco), e em troca recebe juros. Existem diversos tipos de investimentos em renda fixa, cada um com suas próprias características e níveis de risco",
+        link: "https://www.xpi.com.br/produtos/renda-fixa/"
+    },
+    {
+        titulo: "Fundos Imobiliários",
+        descricao: "Fundos Imobiliários (FIIs) são uma modalidade de investimento coletivo que permite aos investidores participarem de um patrimônio constituído por diversos tipos de imóveis ou créditos imobiliários. Ao investir em um FII, o investidor se torna cotista de um fundo e tem direito a receber parte dos lucros gerados pela locação dos imóveis ou pela venda de ativos. Os FIIs são uma excelente opção para quem busca diversificar sua carteira e obter renda através da distribuição de dividendos, que são pagos periodicamente aos cotistas.",
+        link: "https://blog.terrainvestimentos.com.br/fundos-imobiliarios-tudo-o-que-voce-precisa-saber/?utm_term=&utm_campaign=%5BCV%5D+Terra+Investimentos&utm_source=google&utm_medium=cpc&hsa_acc=9967261186&hsa_cam=20553582382&hsa_grp=&hsa_ad=&hsa_src=x&hsa_tgt=&hsa_kw=&hsa_mt=&hsa_net=adwords&hsa_ver=3&gad_source=1&gclid=CjwKCAjwreW2BhBhEiwAavLwfLJtoXzZlbdC6Qq2i6HH4893Z4GqXv_nRIIlnCRyXgBVgieoM4-61xoC9uYQAvD_BwE" 
+    },
+    {
+        titulo: "Bolsa de Valores",
+        descricao: "A bolsa de valores é um mercado organizado onde são negociados diversos tipos de ativos financeiros, como ações, títulos e derivativos. É como um grande leilão, onde empresas e investidores se encontram para comprar e vender esses papéis. Ao investir em ações, por exemplo, o investidor se torna sócio de uma empresa e tem direito a uma parte dos seus lucros. A bolsa de valores é um termômetro da economia, refletindo o otimismo ou pessimismo dos investidores em relação ao futuro. As oscilações nos preços dos ativos são constantes e influenciadas por diversos fatores, como notícias econômicas, políticas e resultados corporativos.",
+        link: "https://conteudos.xpi.com.br/aprenda-a-investir/relatorios/bolsa-de-valores/"
+    },
+];
+
